2c4d54e84fb002e93718d8f230242fc9b2e6a6a0
Diego Ahumada <forsakengmex@gmail.com>
2018-07-09 16:51:04 -0400

    operators on dice roll

--- src/bot.js
+++ src/bot.js
@@ -11,23 +11,41 @@ module.exports = (function () {
 
 	var commands = {
 		"roll": function(param) {
-			var reg = /(\d+)d(\d+)/;
-			var rec = R.map(parseInt, R.tail(reg.exec(param.trim())));
-			if (rec) {
-				var cnt = rec[1];
-				var dice = rec[0];
+			var reg = /(\d+)?d(\d+)?(([\+\-\*])(\d+))?/;
+			var regresult = reg.exec(param.trim());
+			if (regresult) {
+				var rec = R.tail(regresult);
+				var cnt = rec[0] || 1;
+				var dice = rec[1] || 6;
 				var genRoll = (dice) => Math.floor(Math.random() * (dice) + 1);
-				var rolls = R.map(genRoll, R.repeat(cnt, dice));
+				var rolls = R.map(genRoll, R.repeat(dice, cnt));
 				var sum = R.sum(rolls);
+				var rollresult = rolls.toString();
+				
+				var op = rec[3];
+				if (op) {
+					var val = parseInt(rec[4]);
+
+					switch (op) {
+						case "+":
+							sum = sum + val; break;
+						case "*":
+							sum = sum * val; break;
+						case "-":
+							sum = sum - val; break;
+					}
+
+					return Promise.resolve({
+						message: `**Roll result**: [${rollresult}] ${param.trim()} = ${sum}`
+					});
+				}
 				
 				return Promise.resolve({ 
-					message: "*Roll result*: [" +
-							  rolls.toString() + "] sum = " +
-							  sum	
+					message: `**Roll result**: [${rollresult}] sum = ${sum}`
 				});
 			}
 
-			return { error: "Invalid dice (<number>d<number>)" };
+			return Promise.resolve({ error: "Invalid dice (try <number>?d<number>?([+-*]<number>)?)" });
 		},
 		"pk": Encounters.doCmd,
 		"politics": function(cl) {
